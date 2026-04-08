de4712651756f53659e26227f525f64a3517cb39
adityadewhy-local <adityadewhy@gmail.com>
2026-04-08 22:38:01 +0530

    fixed typo

--- app/api/movies/[tmdbId]/status/route.ts
+++ app/api/movies/[tmdbId]/status/route.ts
@@ -41,7 +41,7 @@ export async function GET(
 
 	return NextResponse.json({
 		rating: rating?.score ?? null,
-		inWatchlistItem: !!watchlistItem,
+		inWatchlist: !!watchlistItem,
 		comment: comment ? {id: comment.id, content: comment.content} : null,
 	});
 }
